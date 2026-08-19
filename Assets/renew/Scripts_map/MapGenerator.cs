9fa01a7033438d835ce39ee455fb4b8c73991d29
dlwoduq030329-pixel <dlwoduq030329@gmail.com>
2026-08-19 21:42:34 +0900

    debugMeow
    야옹

--- Assets/renew/Scripts_map/MapGenerator.cs
+++ Assets/renew/Scripts_map/MapGenerator.cs
@@ -64,7 +64,7 @@ public class MapGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("¾ß¿Ë~");
     }
 
     // Update is called once per frame
